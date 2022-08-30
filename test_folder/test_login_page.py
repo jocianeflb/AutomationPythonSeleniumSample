a4c0ec66ff8d34322018b99cc6104ff065355053
Jociane Franzoni de Lima <jfl2@cesar-ldap.intranet>
2022-08-30 18:00:11 -0300

    Change test and and start access page

--- test_folder/test_login_page.py
+++ test_folder/test_login_page.py
@@ -2,6 +2,7 @@
 import unittest
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 import os
 
 class TestLoginPage(unittest.TestCase):
@@ -16,9 +17,12 @@ class TestLoginPage(unittest.TestCase):
         self.driver.implicitly_wait(10)
         self.driver.get(self.base_url)
 
-    def tearDown(self):
-        self.driver.close()
-        self.driver.quit()
+    #def tearDown(self):
+        #self.driver.close()
+        #self.driver.quit()
 
-    def test_access_login_page(self):
+    def test_login_successfull(self):
+        field_username = self.driver.find_element(By.NAME, "username")
+        field_username.click()
+        field_username.send_keys("Admin")
         self.assertTrue(True)
